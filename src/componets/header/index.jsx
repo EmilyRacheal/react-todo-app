fe50eb6f10b394f4521a6cfa7eaa7171722a2712
EmilyRacheal <emilychealy4@gmail.com>
2022-05-14 12:43:16 +0100

    :)

--- src/componets/header/index.jsx
+++ src/componets/header/index.jsx
@@ -4,10 +4,21 @@ import "./header.css";
 
 const Index = () => {
   return (
-    <div>
-      <div>
-        <h1>{/* <BiArchiveIn /> */}</h1>
-        <h1 className="">Emily</h1>
+    <div className="bg-black">
+      <div className="py-20 text-center px-36 todo">
+        <div className="bg-purple-500">
+          <h1 className="py-10 text-3xl text-white">My Daily Tracker</h1>
+          <div className="pt-5 pb-16">
+            <form>
+              <input type="text" name="name" className="px-16 py-4" />
+              <input
+                type="submit"
+                value="Submit"
+                className="py-4 text-white bg-black px-7"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
