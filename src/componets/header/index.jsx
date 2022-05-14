a9c5859da381b0cac22ec863ea4649902e5067eb
EmilyRacheal <emilychealy4@gmail.com>
2022-05-14 19:43:05 +0100

    added a color

--- src/componets/header/index.jsx
+++ src/componets/header/index.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import "./header.css";
-// import BiArchiveIn from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
 
 const Index = () => {
   return (
     <div className="">
       <div className="py-20 text-center px-36 todo">
-        <div className="bg-black rounded-3xl drop-shadow-2xl">
+        <div className="bg-todo rounded-3xl drop-shadow-2xl">
           <h1 className="py-10 text-3xl font-bold text-white">
-            <span className="px-3 pb-1 bg-purple-500">My</span> Daily Tracker
+            <span className="px-3 pb-1 bg-black">My</span> Daily Tracker
           </h1>
-          <div className="pt-5 pb-16">
+          <div className="pt-5">
             <form>
               <input type="text" name="name" className="px-16 py-4 outline-0" />
               <input
                 type="submit"
                 value="Submit"
-                className="py-4 text-white bg-purple-500 px-7"
+                className="py-4 text-white bg-black px-7"
               />
             </form>
+          </div>
+          <div className="px-56 py-20 text-left task">
+            <div className="text-white bg-black">
+              <div className="px-10 py-10">
+                <ul className="flex items-center text-2xl list-disc space-x-28">
+                  <li className="py-3">Hit the gym</li>
+                  <a href="#">
+                    <FaTimes />
+                  </a>
+                </ul>
+                <ul className="flex items-center text-2xl list-disc space-x-28">
+                  <li className="">Hit the gym</li>
+                  <a href="#">
+                    <FaTimes />
+                  </a>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/componets/taskList/index.jsx
+++ src/componets/taskList/index.jsx
@@ -4,16 +4,19 @@ import "./taskList.css";
 const Index = () => {
   return (
     <div>
-      <div className="px-56 py-20 text-left">
+      {/* <div className="px-56 py-20 text-left task">
         <div className="bg-purple-500">
           <div className="px-10 py-10">
-            <ul className="flex text-2xl list-disc ">
+            <ul className="flex items-center text-2xl list-disc">
               <li className="py-3">Hit the gym</li>
-              <span>+</span>
+              <div className="justify-items-center">
+                <div class="w-20 h-10 bg-sky-400 opacity-20"></div>
+                <div class="-mt-6 bg-black text-white text-center ">+</div>
+              </div>
             </ul>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
