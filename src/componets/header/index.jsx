7e4c51c88eb2fd1fff71220ed02cd5b8cae56ea8
EmilyRacheal <emilychealy4@gmail.com>
2022-05-14 12:20:16 +0100

    first commit

---
+++ src/componets/header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+// import BiArchiveIn from "react-icons/bi";
+
+const Index = () => {
+  return (
+    <div>
+      <div>
+        <h1>{/* <BiArchiveIn /> */}</h1>
+        <h1 className="">Emily</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
